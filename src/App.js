3a4cd39ca1665d47df6f7fa8285bb01816d7ea4e
Amberside <47558882+Amberside@users.noreply.github.com>
2020-09-16 09:55:45 +1000

    Create AppNavBar and add to App.js
    Created AppNavBar and imported to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+// import of React and needed components
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Container } from 'react-bootstrap'
+
+
+// Import of custom components
+import AppNavBar from './components/layout/AppNavBar';
 
 const App = () => {
   return (
-    <div>
-      <h1>Client Panel</h1>
-    </div>
+    <Router>
+      <AppNavBar />
+      <Container>
+        
+      </Container>
+    </Router>
   );
 }
 
