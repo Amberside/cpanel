ec4f80bb0ef54e616189397779655ea35f5bbd98
Amberside <47558882+Amberside@users.noreply.github.com>
2020-09-16 11:16:36 +1000

    Create Clients.js
    Added the Client component

---
+++ src/components/clients/Clients.js
@@ -0,0 +1,62 @@
+// rafcp
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Row, Col, Table } from 'react-bootstrap';
+
+import Spinner from '../layout/Spinner';
+
+const Clients = props => {
+  
+  const clients = [
+    { id: '1', firstName: 'Harry', lastName: 'Riney', email: 'harry@test.com', balance: 45 },
+    { id: '2', firstName: 'Jill', lastName: 'Bloggs', email: 'bloggys@test.com', balance: 87 },
+    { id: '3', firstName: 'Patrick', lastName: 'McNee', email: 'neey@test.com', balance: 93 }
+  ]
+  
+  return (
+    <Fragment>
+      <Row>
+        <Col md={6}>
+          <h2>Clients</h2>
+        </Col>
+        <Col md={6}>
+          <h5 className='text-right text-secondary'>
+            Total Owed: $274
+          </h5>
+        </Col>
+      </Row>
+      <Table striped>
+        <thead className='thead-inverse'>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Balance</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          { clients.map(client => (
+            <tr key={client.id}>
+              <td>{client.firstName}{' '}{client.lastName}</td>
+              <td>{client.email}</td>
+              <td>{client.balance}</td>
+              <td>
+                <Link to={`/client/${client.id}`}
+                  className="btn btn-secondary text-light">
+                    Details
+                </Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Fragment>
+  )
+}
+
+Clients.propTypes = {
+  clients: PropTypes.array,
+}
+
+export default Clients
